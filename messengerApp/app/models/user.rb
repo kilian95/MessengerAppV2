83f6bf4c883a2e1811e796275cf1031b2f9dad47
Kilian Drayne <kilian.drayne@sla-ltd.com>
2017-08-08 16:24:24 +0100

    create messaging app without action cable

--- messengerApp/app/models/user.rb
+++ messengerApp/app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+	has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
+	has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
+	has_many :personal_messages, dependent: :destroy       
 end
--- messengerApp/config/routes.rb
+++ messengerApp/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
- 
+  root 'conversations#index'
+
+  resources :users, only: [:index]
+  resources :personal_messages, only: [:new, :create]
+	resources :conversations, only: [:index, :show]
   
   
 end
